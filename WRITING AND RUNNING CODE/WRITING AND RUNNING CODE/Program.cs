9ff41065bfcb40c88be00cdb546ae6fe4acc2471
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-03 13:27:47 -0700

    Basic Exercise 1

---
+++ WRITING AND RUNNING CODE/WRITING AND RUNNING CODE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WRITING_AND_RUNNING_CODE
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.Read();
+        }
+    }
+}

553c0484e1b4bb7c4b812d2348e62ac44a9c548a
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-04 12:23:38 -0700

    Console Student Daily Report

---
+++ Exercise_2/Exercise_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string Course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string Page = Console.ReadLine();
+            int current_page = Convert.ToInt32(Page);
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            string help = Console.ReadLine();
+            bool Help = Convert.ToBoolean(help);
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string Shared_experience = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string Feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hours = Console.ReadLine();
+            int study_hours = Convert.ToInt32(hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

06d388e1d782d4b346727b6cd26eaf3ab341997b
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-04 18:57:33 -0700

    Do-While Loops

---
+++ Exercise_8/Exercise_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 1;
+            do
+            {
+                Console.WriteLine("value of a: {0}", count);
+                count = count + 1;
+            }
+            while (count < 20);
+            Console.ReadLine();
+        }
+    }
+} 
+

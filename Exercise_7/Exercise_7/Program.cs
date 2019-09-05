81f28f493a770c13d58e68059dfe95c674a586ff
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-04 18:47:04 -0700

    While Loop

---
+++ Exercise_7/Exercise_7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool morecandy = true;
+            int count = 0;
+
+            while (morecandy)
+            {
+                Console.WriteLine("Yes or No: Would you like another peice of candy?");
+
+                string answer = Console.ReadLine();
+                if (answer == "Yes")
+                {
+                    int total_candycount = count++;
+                    Console.WriteLine(total_candycount);
+                }
+                else
+                {
+                    morecandy = false;
+                    Console.WriteLine("Its about time...");
+                }
+            }
+        }
+    }
+}

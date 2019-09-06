b907b4600d258446788ede543303683fefb84545
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-06 15:45:41 -0700

    Calling on Methods

---
+++ Exercise_13/Exercise_13/Operator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_13
+{
+    class Operator
+    {
+        public static int Add_op(int number)
+        {
+            int numb = number + 2;
+            Console.WriteLine(numb);
+            return numb;
+        }
+        public static int Subtract_op(int number)
+        {
+            int numb = number - 2;
+            Console.WriteLine(numb);
+            return numb;
+        }
+        public static int Divide_op(int number)
+        {
+            int numb = number / 2;
+            Console.WriteLine(numb);
+            return numb;
+            
+        }
+
+    }
+}
---
+++ Exercise_13/Exercise_13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number (" + number + ") added by 2:");
+            Operator.Add_op(number);
+
+            Console.WriteLine("Your number (" + number + ") subtracted by 2:");
+            Operator.Subtract_op(number);
+
+            Console.WriteLine("Your number (" + number + ") divided by 2:");
+            Operator.Divide_op(number);
+
+            Console.ReadLine();
+        }
+    }
+}

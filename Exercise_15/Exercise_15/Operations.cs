4d5f3223374d35fcfbf089f86886dc49fcaea922
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-06 18:27:18 -0700

    multiple methods

---
+++ Exercise_15/Exercise_15/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_15
+{
+    class Operations
+    {
+        public void operation(int number1)
+        {
+            int numb = number1 + 2;
+            Console.WriteLine("Your number, added by 2 is: ");
+            Console.WriteLine(numb);
+        }
+        public void operation(decimal number2)
+        {
+            decimal numb = number2 + 2.5M;
+            Console.WriteLine("Your decimal number, added by 2.5 is: ");
+            Console.WriteLine(numb);
+        }
+        public void operation(string number3)
+        {
+            int numb = Convert.ToInt32(number3);
+            int value = numb + 2;
+            Console.WriteLine("Your string value, added by 2 is: ");
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Exercise_15/Exercise_15/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a decimal number: ");
+            decimal number2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            string number3 = Console.ReadLine();
+
+            Operations op = new Operations();
+            op.operation(number1);
+            
+
+            Operations opp = new Operations();
+            opp.operation(number2);
+
+            Operations Opp = new Operations();
+            Opp.operation(number3);
+
+            Console.ReadLine();
+
+        }
+    }
+}

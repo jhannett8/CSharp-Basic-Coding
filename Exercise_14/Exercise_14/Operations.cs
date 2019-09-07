16b2298317f488027af10a861391e4aba31565c2
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-06 17:37:30 -0700

    Void Method

---
+++ Exercise_14/Exercise_14/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_14
+{
+    class Operations
+    {
+        public void operation(int number1, int number2)
+        {
+            int numb = number1 + 2;
+            Console.WriteLine("Your number, added by 2 is: ");
+            Console.WriteLine(numb);
+
+            Console.WriteLine("Your Second number is: ");
+            Console.WriteLine(number2);
+        }
+    }
+}
---
+++ Exercise_14/Exercise_14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Operations op = new Operations();
+            op.operation(number1, number2);
+
+        }
+    }
+}

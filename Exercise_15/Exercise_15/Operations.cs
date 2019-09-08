2d897cc7af58094479b97d88da30ea04e0f8434a
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-08 15:14:20 -0700

    modify return statement in modules

--- Exercise_15/Exercise_15/Operations.cs
+++ Exercise_15/Exercise_15/Operations.cs
@@ -6,24 +6,22 @@ namespace Exercise_15
 {
     class Operations
     {
-        public void operation(int number1)
+        public int operation(int number1)
         {
             int numb = number1 + 2;
-            Console.WriteLine("Your number, added by 2 is: ");
-            Console.WriteLine(numb);
+            return numb;
+            
         }
-        public void operation(decimal number2)
+        public decimal operation(decimal number2)
         {
             decimal numb = number2 + 2.5M;
-            Console.WriteLine("Your decimal number, added by 2.5 is: ");
-            Console.WriteLine(numb);
+            return numb;
         }
-        public void operation(string number3)
+        public int operation(string number3)
         {
             int numb = Convert.ToInt32(number3);
             int value = numb + 2;
-            Console.WriteLine("Your string value, added by 2 is: ");
-            Console.WriteLine(value);
+            return value;
         }
     }
 }
--- Exercise_15/Exercise_15/Program.cs
+++ Exercise_15/Exercise_15/Program.cs
@@ -8,20 +8,24 @@ namespace Exercise_15
         {
             Console.WriteLine("Please enter a number: ");
             int number1 = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Please enter a decimal number: ");
             decimal number2 = Convert.ToDecimal(Console.ReadLine());
+
             Console.WriteLine("Please enter another number: ");
             string number3 = Console.ReadLine();
 
             Operations op = new Operations();
-            op.operation(number1);
+            int Value1 = op.operation(number1);
+            Console.WriteLine(number1 + " + 2 = " + Value1);
             
-
             Operations opp = new Operations();
-            opp.operation(number2);
+            decimal Value2 = opp.operation(number2);
+            Console.WriteLine(number2 + " + 2.5 = " + Value2);
 
             Operations Opp = new Operations();
-            Opp.operation(number3);
+            int Value3 = Opp.operation(number3);
+            Console.WriteLine(number3 + " + 2 = " + Value3);
 
             Console.ReadLine();
 

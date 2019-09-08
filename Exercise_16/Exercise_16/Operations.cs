daa74ced267eb3de31ffbf6fde9b0e8aab4d95a0
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-08 16:31:35 -0700

    Optional Arguments

---
+++ Exercise_16/Exercise_16/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_16
+{
+    class Operations
+    {
+        public int operation(int required, int optionalint = 1)
+        {
+            
+            int value = required + optionalint;
+            Console.WriteLine(required + " + " + optionalint + " = " + value);
+            return value;
+        }
+    }
+}
---
+++ Exercise_16/Exercise_16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Required: Please log a number: ");
+            int required = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optional: Please log another number: ");
+            try
+            {
+                int optionalint = Convert.ToInt32(Console.ReadLine());
+                Operations op = new Operations();
+                op.operation(required, optionalint);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Operations op = new Operations();
+                op.operation(required);
+                Console.ReadLine();
+            }
+        }
+    }
+}

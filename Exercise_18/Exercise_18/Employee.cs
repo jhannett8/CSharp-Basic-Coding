6a0155a996f5f522165b64bfd4f5d3910648e570
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-09 13:31:26 -0700

    Inheritance

---
+++ Exercise_18/Exercise_18/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_18
+{
+    public class Employee : Person 
+    {
+        public int id { get; set; }
+    }
+}
---
+++ Exercise_18/Exercise_18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Joe";
+            employee.LastName = "Hannett";
+            employee.SayName();
+        }
+    }
+}

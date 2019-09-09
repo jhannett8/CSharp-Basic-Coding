76ebd8fcaa757d5aadb9f86e654d4d0ef47db56f
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-09 15:44:19 -0700

    abstract class

---
+++ Exercise_19/Exercise_19/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_19
+{
+    class Employee :Person
+    {
+        
+    }
+}
---
+++ Exercise_19/Exercise_19/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Joe";
+            employee.LastName = "Hannett";
+            employee.SayName();
+        }
+    }
+}

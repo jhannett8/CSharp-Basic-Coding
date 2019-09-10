431f236c1db287c1055c45e13ada1b501b70cc49
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-10 16:16:00 -0700

    Generics

---
+++ Exercise_22/Exercise_22/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_22
+{
+    public class Employee<T> 
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public List<T> Things { get; set; } 
+    }
+}
---
+++ Exercise_22/Exercise_22/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Employee<string> ListString = new Employee<string>();
+                ListString.Things = new List<string>() { "Jeff Bezos", "Bill Gates", "Tony Stark", "Andrew Garfeild", "Ricky Sanchez" };
+
+                foreach (string Thing in ListString.Things)
+                {
+                    Console.WriteLine(Thing);
+                }
+
+                Employee<int> ListInt = new Employee<int>();
+                ListInt.Things = new List<int>() { 0, 1, 2, 3, 4 };
+
+                foreach (int Thing in ListInt.Things)
+                {
+                    Console.WriteLine(Thing);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}

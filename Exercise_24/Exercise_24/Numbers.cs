518acd4cd823c628b3232f58dfcb88bb7a94c092
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-10 18:24:31 -0700

    Struct

---
+++ Exercise_24/Exercise_24/Numbers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_24
+{
+    public struct Numbers
+    {
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Exercise_24/Exercise_24/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers Number = new Numbers();
+            Number.Amount = 4.3M;
+            Console.WriteLine("The following decimal has been assigned to \"Number\" struct: {0}", Number.Amount);
+            Console.ReadLine();
+        }
+    }
+}

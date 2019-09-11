bd982648485b01745b9c2c36ac5bd77be0d54178
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-10 17:40:39 -0700

    Enum

---
+++ Exercise_23/Exercise_23/DayCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_23
+{
+    class DayCalculator
+    {
+        public DayOfWeek DayOfWeek { get; }
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ Exercise_23/Exercise_23/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World! What day is today?");
+                string day = Console.ReadLine();
+                DayOfWeek today = DateTime.Today.DayOfWeek;
+
+                if (day == (Convert.ToString(today)))
+                {
+                    Console.WriteLine("Your right! Today is {0}!", day);
+                }
+                else
+                {
+                    Console.WriteLine("Please select the correct day of the week.");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Please select the correct day of the week.");
+            }
+        }
+    }
+}

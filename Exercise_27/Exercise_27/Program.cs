2a0194a96607b2ed9f310c8a3c8a6ff40f85ef97
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-13 10:20:33 -0700

    DateTime

---
+++ Exercise_27/Exercise_27/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Today's date is: {0}", today);
+            Console.WriteLine("Please input the amount of hours you would like to add to today's current time; ");
+            double hours = Convert.ToDouble(Console.ReadLine());
+            DateTime NewTime= today.AddHours(hours);
+            Console.WriteLine("The new time is; {0}", NewTime);
+        }
+    }
+}

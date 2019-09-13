840c3541695770cdd5c04f2052503ae45cf3bdf8
Joseph Hannett <hannettjoseph@gmail.com>
2019-09-13 16:28:42 -0700

    Const Variables, Var, Constructor Chains

---
+++ Exercise_28/Exercise_28/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a const variable
+            const int Months = 12;
+            Console.WriteLine("const holds the in = {0}", Months);
+            //create a variable with the keyword var
+            var age = 25;
+            Console.WriteLine("var holds an int = {0}", age);
+        }
+    }
+}
---
+++ Exercise_28/Exercise_28/SampleConstructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_28
+{
+    class Employee
+    {
+        //Chaining Constructors 
+        public Employee(string id)
+            : this(id, "", "")
+        {
+
+        }
+
+        public Employee(string id, string fName)
+            : this(id, fName, "")
+        {
+
+        }
+
+        public Employee(string id, string fName, string lName)
+        {            
+
+            string _id = id;
+            string _fName = fName;
+            string _lName = lName;
+        }
+    }
+}
